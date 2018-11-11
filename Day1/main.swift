e942af1017ae2362df3910ffb3873f4f163c487c
Levente Vig <vig.levente@codeyard.eu>
2018-11-11 17:17:38 +0100

    ✨ Added day 1 sub project

---
+++ Day1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day1
+//
+//  Created by Levente Vig on 2018. 11. 11..
+//  Copyright © 2018. Levente Vig. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
