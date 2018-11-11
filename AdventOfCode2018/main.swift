94aab4c3a600ed19da725298386cab37791be605
Levente Vig <vig.levente@codeyard.eu>
2018-11-11 17:14:29 +0100

    initial commit

---
+++ AdventOfCode2018/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2018
+//
+//  Created by Levente Vig on 2018. 11. 11..
+//  Copyright © 2018. Levente Vig. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
