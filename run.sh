72389e7a31545b4ccce17e34efd0d24af17f72f0
Levente Vig <levivig@gmail.com>
2018-12-02 00:46:29 +0100

    🐛 Fixed run script
    chmod +x was pointing at the wrong file

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ case $# in
         app="day$1"
         folder="Day$1"
         swiftc -o ./$folder/$app ./$folder/main.swift ./AdventOfCode2018/Utilities.swift
-        chmod +x $app
+        chmod +x ./$folder/$app
         ./$folder/$app;;
     *)
         echo "usage: ./run.sh {n}, where n is the day you want to run"
