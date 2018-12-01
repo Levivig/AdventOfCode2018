c0ec7b91ecce385dee47a7768911895830b3fd0c
Levente Vig <vig.levente@codeyard.eu>
2018-12-01 10:53:36 +0100

    ✨ Preparing for Day 2.
    Added new target.

---
+++ Day2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Day2
+//
+//  Created by Levente Vig on 2018. 12. 01..
+//  Copyright © 2018. Levente Vig. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
