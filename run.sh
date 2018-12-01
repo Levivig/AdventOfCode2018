86619a21d3a43a4abfc881cd014b0ad6944903db
Levente Vig <levivig@gmail.com>
2018-12-01 23:16:09 +0100

    💻 Added cli run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+case $# in
+    1)
+        app="day$1"
+        folder="Day$1"
+        swiftc -o ./$folder/$app ./$folder/main.swift ./AdventOfCode2018/Utilities.swift
+        chmod +x $app
+        ./$folder/$app;;
+    *)
+        echo "usage: ./run.sh {n}, where n is the day you want to run"
+        exit;;
+esac
